38f94233eb1969c0a382fb88343e42aa900c0420
Anton Angelov <aangelov@automatetheplanet.com>
2022-05-08 13:42:54 +0300

    chapter 9

---
+++ chapter9-cdp-performancemetrics/src/test/java/CapturePerformanceMetricsTests.java
@@ -0,0 +1,129 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.devtools.DevTools;
+import org.openqa.selenium.devtools.HasDevTools;
+import org.openqa.selenium.devtools.v96.performance.Performance;
+import org.openqa.selenium.devtools.v96.performance.model.Metric;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.Optional;
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.openqa.selenium.remote.http.Contents.utf8String;
+
+public class CapturePerformanceMetricsTests {
+    private final int WAIT_FOR_ELEMENT_TIMEOUT = 30;
+    private WebDriver driver;
+    private WebDriverWait webDriverWait;
+    private Actions actions;
+
+    @BeforeAll
+    public static void setUpClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(WAIT_FOR_ELEMENT_TIMEOUT));
+        actions = new Actions(driver);
+    }
+
+    @ParameterizedTest(name = "{index}. verify todo list created successfully when technology = {0}")
+    @ValueSource(strings = {
+            "Backbone.js",
+            "AngularJS",
+
+    })
+    public void verifyToDoListCreatedSuccessfully_withParams(String technology){
+        System.out.println("###############################");
+        System.out.println(String.format("\nGET METRICS for %s\n", technology));
+        var devToolsDriver = (HasDevTools)driver;
+        try (DevTools devTools = devToolsDriver.getDevTools()) {
+            devTools.createSession();
+            devTools.send(Performance.enable(Optional.empty()));
+
+
+            driver.navigate().to("https://todomvc.com/");
+            openTechnologyApp(technology);
+            for (int i = 0; i <= 99; i += 1) {
+                String randomString = generateRandomString();
+                addNewToDoItem(randomString);
+                getItemCheckbox(randomString).click();
+            }
+
+            List<Metric> metricList = devTools.send(Performance.getMetrics());
+            for(Metric m : metricList) {
+                System.out.println(m.getName() + " = " + m.getValue());
+            }
+
+            System.out.println("###############################");
+        }
+    }
+
+    private String generateRandomString() {
+        int leftLimit = 97; // letter 'a'
+        int rightLimit = 122; // letter 'z'
+        int targetStringLength = 10;
+        Random random = new Random();
+
+        String generatedString = random.ints(leftLimit, rightLimit + 1)
+                .limit(targetStringLength)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
+
+        return generatedString;
+    }
+
+    private void assertLeftItems(int expectedCount){
+        var resultSpan = waitAndFindElement(By.xpath("//footer/*/span | //footer/span"));
+        if (expectedCount == 1){
+            var expectedText = String.format("%d item left", expectedCount);
+            validateInnerTextIs(resultSpan, expectedText);
+        } else {
+            var expectedText = String.format("%d items left", expectedCount);
+            validateInnerTextIs(resultSpan, expectedText);
+        }
+    }
+
+    private void validateInnerTextIs(WebElement resultElement, String expectedText){
+        webDriverWait.until(ExpectedConditions.textToBePresentInElement(resultElement, expectedText));
+    }
+
+    private WebElement getItemCheckbox(String todoItem){
+        var xpathLocator = String.format("//label[text()='%s']/preceding-sibling::input", todoItem);
+        return waitAndFindElement(By.xpath(xpathLocator));
+    }
+
+    private void openTechnologyApp(String technologyName){
+        var technologyLink = waitAndFindElement(By.linkText(technologyName));
+        technologyLink.click();
+    }
+
+    private void addNewToDoItem(String todoItem){
+        var todoInput = waitAndFindElement(By.xpath("//input[@placeholder='What needs to be done?']"));
+        todoInput.sendKeys(todoItem);
+        actions.click(todoInput).sendKeys(Keys.ENTER).perform();
+    }
+
+    private WebElement waitAndFindElement(By locator){
+        return webDriverWait.until(ExpectedConditions.presenceOfElementLocated(locator));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
---
+++ chapter9-cdp-performancemetrics/src/test/java/DevToolsCapturePerformanceMetricsLambdaTestTests.java
@@ -0,0 +1,177 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.openqa.selenium.*;
+import org.openqa.selenium.devtools.DevTools;
+import org.openqa.selenium.devtools.HasDevTools;
+import org.openqa.selenium.devtools.v96.performance.Performance;
+import org.openqa.selenium.devtools.v96.performance.model.Metric;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.Augmenter;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.openqa.selenium.remote.http.Contents.utf8String;
+
+public class DevToolsCapturePerformanceMetricsLambdaTestTests {
+    private final int WAIT_FOR_ELEMENT_TIMEOUT = 30;
+    private WebDriver driver;
+    private WebDriverWait webDriverWait;
+    private Actions actions;
+
+    @BeforeAll
+    public static void setUpClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    public void setUp() throws MalformedURLException {
+
+    }
+
+    @ParameterizedTest(name = "{index}. verify todo list created successfully when technology = {0}")
+    @ValueSource(strings = {
+            "Backbone.js",
+            "AngularJS",
+            "React",
+            "Vue.js",
+            "CanJS",
+            "Ember.js",
+            "KnockoutJS",
+            "Marionette.js",
+            "Polymer",
+            "Angular 2.0",
+            "Dart",
+            "Elm",
+            "Closure",
+            "Vanilla JS",
+            "jQuery",
+            "cujoJS",
+            "Spine",
+            "Dojo",
+            "Mithril",
+            "Kotlin + React",
+            "Firebase + AngularJS",
+            "Vanilla ES6"
+    })
+    public void measurePerformanceFor(String technology) throws MalformedURLException {
+        initializeSession("measurePerformanceFor" + technology);
+
+        System.out.println("###############################");
+        System.out.println(String.format("\nGET METRICS for %s\n", technology));
+        var devToolsDriver = (HasDevTools)driver;
+        try (DevTools devTools = devToolsDriver.getDevTools()) {
+            devTools.createSession();
+            devTools.send(Performance.enable(Optional.empty()));
+
+
+            driver.navigate().to("https://todomvc.com/");
+            openTechnologyApp(technology);
+            for (int i = 0; i <= 99; i += 1) {
+                String randomString = generateRandomString();
+                addNewToDoItem(randomString);
+                getItemCheckbox(randomString).click();
+            }
+
+            List<Metric> metricList = devTools.send(Performance.getMetrics());
+            for(Metric m : metricList) {
+                System.out.println(m.getName() + " = " + m.getValue());
+            }
+
+            System.out.println("###############################");
+        }
+    }
+
+    private void initializeSession(String testName) throws MalformedURLException {
+        String username = System.getenv("LT_USERNAME");
+        String authkey = System.getenv("LT_ACCESSKEY");
+        String hub = "@hub.lambdatest.com/wd/hub";
+
+        var capabilities = new DesiredCapabilities();
+        capabilities.setCapability("browserName", "Chrome");
+        capabilities.setCapability("browserVersion", "latest");
+        HashMap<String, Object> ltOptions = new HashMap<String, Object>();
+        ltOptions.put("user", username);
+        ltOptions.put("accessKey", authkey);
+        ltOptions.put("build", "Selenium 4");
+        ltOptions.put("name",testName);
+        ltOptions.put("platformName", "Windows 10");
+        ltOptions.put("console", true);
+        ltOptions.put("performance", true);
+        ltOptions.put("seCdp", true);
+        ltOptions.put("selenium_version", "4.0.0");
+        capabilities.setCapability("LT:Options", ltOptions);
+
+        driver = new RemoteWebDriver(new URL("https://" + username + ":" + authkey + hub), capabilities);
+        Augmenter augmenter = new Augmenter();
+        driver = augmenter.augment(driver);
+        webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(WAIT_FOR_ELEMENT_TIMEOUT));
+        actions = new Actions(driver);
+        driver.manage().window().maximize();
+    }
+
+    private String generateRandomString() {
+        int leftLimit = 97; // letter 'a'
+        int rightLimit = 122; // letter 'z'
+        int targetStringLength = 10;
+        Random random = new Random();
+
+        String generatedString = random.ints(leftLimit, rightLimit + 1)
+                .limit(targetStringLength)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
+
+        return generatedString;
+    }
+
+    private void assertLeftItems(int expectedCount){
+        var resultSpan = waitAndFindElement(By.xpath("//footer/*/span | //footer/span"));
+        if (expectedCount == 1){
+            var expectedText = String.format("%d item left", expectedCount);
+            validateInnerTextIs(resultSpan, expectedText);
+        } else {
+            var expectedText = String.format("%d items left", expectedCount);
+            validateInnerTextIs(resultSpan, expectedText);
+        }
+    }
+
+    private void validateInnerTextIs(WebElement resultElement, String expectedText){
+        webDriverWait.until(ExpectedConditions.textToBePresentInElement(resultElement, expectedText));
+    }
+
+    private WebElement getItemCheckbox(String todoItem){
+        var xpathLocator = String.format("//label[text()='%s']/preceding-sibling::input", todoItem);
+        return waitAndFindElement(By.xpath(xpathLocator));
+    }
+
+    private void openTechnologyApp(String technologyName){
+        var technologyLink = waitAndFindElement(By.linkText(technologyName));
+        technologyLink.click();
+    }
+
+    private void addNewToDoItem(String todoItem){
+        var todoInput = waitAndFindElement(By.xpath("//input[@placeholder='What needs to be done?']"));
+        todoInput.sendKeys(todoItem);
+        actions.click(todoInput).sendKeys(Keys.ENTER).perform();
+    }
+
+    private WebElement waitAndFindElement(By locator){
+        return webDriverWait.until(ExpectedConditions.presenceOfElementLocated(locator));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
